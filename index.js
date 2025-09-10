455bbddbc0030e97f08ad43cae12d3cd5ceab7e7
JAYPATEL1423 <jaypatel.shivinfotech@gmail.com>
2025-09-10 11:36:23 +0530

    Fix: Bug Resolved

--- index.js
+++ index.js
@@ -39,3 +39,18 @@ projectMenuItems.forEach((item) => {
     });
   });
 });
+
+// navbar close
+
+document.addEventListener("DOMContentLoaded", function () {
+  const navLinks = document.querySelectorAll(".navbar-nav .nav-link");
+  const navbarCollapse = document.getElementById("navbarNav");
+
+  navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      if (navbarCollapse.classList.contains("show")) {
+        new bootstrap.Collapse(navbarCollapse).hide();
+      }
+    });
+  });
+});
